6eb257b284bcf8e70b1009fe00baf6dc383001ab
Yang tao <emcse@163.com>
2012-11-15 18:11:58 +0800

    add create

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,10 +5,25 @@ class PlantsController < ApplicationController
   
   def show
     @plant = Plant.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+    end
   end
   
   def new
     @plant = Plant.new
+  end
+  
+  def create
+    @plant = Plant.new(params[:plant])
+    if @plant.save
+      respond_to do |format|
+        format.html { redirect_to plants_path }
+      end
+    else
+      render :action => "new"
+    end
   end
   
   def edit
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,8 +1,8 @@
 class Plant < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :zw, :dlw, :rqzw, :ke, :shu, :zwzym, :stsyx, :gsyxdj, :cdxzzb, :stfwzhql, :jwzsql, :gtsyql, :zcql, :kxso, :xkcl, :kxfhw, :kxo, :kxzjs, :kxyj, :gjql, :cl, :xzd, :gsbw, :gssx, :gsq, :zzgd, :jdsz, :zrfbqk, :dx, :vocpflgwz
   def self.search(search)
     if search
-      where("zw LIKE ?", "%#{search}%")
+      where("zw LIKE ?", :"%#{search}%")
     else
       all
     end
